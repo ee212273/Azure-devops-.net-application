df74cdbfa233d0dbc631639e536d67acf4a3caa6
ramu <tramu5152@gmail.com>
2025-10-17 11:03:06 +0530

    .net Mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mvc_project_._net_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
